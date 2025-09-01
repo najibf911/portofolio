c4484ed8c39d635dd146aeae9091ccc573711477
Muhammad Najib F <56527849+najibf911@users.noreply.github.com>
2025-09-01 13:13:24 +0000

    commit

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+// hamburger
+// variable
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
